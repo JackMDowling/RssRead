c1ac4a31c7241035c44d62444ae51acd3171579e
Jack <jack.m.dowling26@gmail.com>
2022-10-12 12:11:47 -0600

    working on rss

--- src/helpers/getRss.js
+++ src/helpers/getRss.js
@@ -2,18 +2,26 @@ const axios = require('axios');
 const convert = require('xml-js');
 
 function getRss() {
-  try {
-    axios.get('https://www.theguardian.com/sport/mlb/rss').then((res) => {
-      const jsonStringData = convert.xml2json(res.data, {
+  const url = `https://www.theguardian.com/sport/mlb/rss`;
+
+  fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+    .then((response) => response.text())
+    .then((str) => {
+      const jsonStringData = convert.xml2json(str, {
         compact: true,
         spaces: 4,
       });
       const jsonObjData = JSON.parse(jsonStringData);
       return jsonObjData;
+    })
+    .then((data) => {
+      return data;
+      //   // const jsonStringData = convert.xml2json(data, {
+      //   //   compact: true,
+      //   //   spaces: 4,
+      //   // });
+      //   // console.log('data here', jsonStringData);
     });
-  } catch (error) {
-    console.log(error);
-  }
 }
 
 module.exports = { getRss };
