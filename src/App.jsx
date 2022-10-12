98b32a46706cfd2d31621b9a0fc9acc122ff3ac7
Jack <jack.m.dowling26@gmail.com>
2022-10-12 09:23:11 -0600

    webpack and npm init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='hello-world'>
+      <h1>Hello World</h1>
+      <p>Getting started</p>
+    </div>
+  )
+}
+export default App
