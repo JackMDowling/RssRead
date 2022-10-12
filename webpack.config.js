80dda552e6eaf149f53f6f53df9177784a22541b
Jack <jack.m.dowling26@gmail.com>
2022-10-12 11:54:54 -0600

    quicksave

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = {
   watch: true,
